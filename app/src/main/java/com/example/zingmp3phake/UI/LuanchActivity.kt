99f5d4d23182bea9f1f8e2501dcf831d7bc11e19
truongtv-1281 <tran.van.truong@sun-asterisk.com>
2022-08-11 10:00:57 +0700

    init project

---
+++ app/src/main/java/com/example/zingmp3phake/UI/LuanchActivity.kt
@@ -0,0 +1,20 @@
+package com.example.zingmp3phake.UI
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zingmp3phake.R
+import com.example.zingmp3phake.utils.TIME_DELAY_LAUNCH
+import com.example.zingmp3phake.utils.handler
+
+class LuanchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_luanch)
+        handler.postDelayed({
+            val intent = Intent(this, MainAppActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_DELAY_LAUNCH)
+    }
+}
